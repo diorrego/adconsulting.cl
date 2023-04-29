0aa312312057c042a2aa27fabef958fe9f2b53e3
dior <diorrego1@gmail.com>
2023-04-29 02:49:49 -0400

    :art: change import globals.css and add postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
+const purgecss = {
+  '@fullhuman/postcss-purgecss': {
+    content: [
+      './components/**/*.ts',
+      './pages/**/*.ts',
+      './components/**/*.tsx',
+      './pages/**/*.tsx',
+    ],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+};
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    // Purge and minify CSS only production builds only
+    ...(process.env.postCSS === 'production'
+      ? { ...purgecss, cssnano: {} }
+      : {}),
   },
-}
+};
